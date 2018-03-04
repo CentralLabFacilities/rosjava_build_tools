071cbcc7d8811a721de690886120576928481440
Johannes Kummert <jkummert@techfak.uni-bielefeld.de>
2018-03-04 13:02:45 +0100

    use black/white-list

--- src/rosjava_build_tools/catkin.py
+++ src/rosjava_build_tools/catkin.py
@@ -64,8 +64,8 @@ def index_message_package_dependencies_from_local_environment(package_name_list=
     # The following returns: A list of tuples containing the relative path and a ``Package`` object,
     sorted_package_tuples = catkin_pkg.topological_order.topological_order_packages(
                                 packages=dict(message_packages.values()),
-                                whitelisted=None,
-                                blacklisted=None,
+                                whitelisted=message_package_whitelist,
+                                blacklisted=message_package_blacklist,
                                 underlay_packages=dict(all_packages.values()))
     # print("%s" % [p.name for (unused_relative_path, p) in sorted_package_tuples])
     return sorted_package_tuples
